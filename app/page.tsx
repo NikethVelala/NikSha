edcf02be3fa7d38c15154f6547dfb221f868d939
NikethVelala <100532831+NikethVelala@users.noreply.github.com>
2026-09-05 11:00:38 +0530

    feat: add elegant wedding navigation
    * feat: add elegant wedding navigation
    
    * feat: add navigation to wedding page
    
    * feat: include footer with wedding navigation

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Navigation from "@/components/Navigation/Navigation";
 import Hero from "@/components/Hero/Hero";
 import Welcome from "@/components/Welcome/Welcome";
 import Story from "@/components/Story/Story";
@@ -10,6 +11,8 @@ import Footer from "@/components/Footer/Footer";
 export default function Home() {
   return (
     <>
+      <div id="top" />
+      <Navigation />
       <Hero />
       <Welcome />
       <Story />
